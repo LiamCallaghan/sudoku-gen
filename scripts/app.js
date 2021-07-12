72276d93547a720683d6b115a3c4e55a66237a8f
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-12 10:27:40 +0100

    boxes

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,18 @@ function init() {
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
+      // console.log((i / 3) % 1 )
+      // cell.classList.add(i)
+      
+      if ((i / 3) % 1 === 0) {
+        cell.classList.add('left')
+      } 
+      if (((i + 1) / 9) % 1 === 0) {
+        cell.classList.add('right')
+      }
+      if ((i > 17 && i < 27) || (i > 44 && i < 54)) {
+        cell.classList.add('bottom')
+      }
       grid.appendChild(cell)
       cells.push(cell)
     }
