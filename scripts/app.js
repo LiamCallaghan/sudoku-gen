1b02808ff5fa811059c8b9a724ad51ca2934aa34
Liam Callaghan <ljcallaghan64@gmail.com>
2021-07-13 16:46:22 +0100

    random number up to nine in squares

--- scripts/app.js
+++ scripts/app.js
@@ -5,15 +5,33 @@ function init() {
   const cellCount = width * width
   let nineCount = 1
   let rowCount = 1
-  // const row1 = []
-  // const row2 = []
-  // const row3 = []
-  // const row4 = []
-  // const row5 = []
-  // const row6 = []
-  // const row7 = []
-  // const row8 = []
-  // const row9 = []
+
+  function rowMaker() {
+
+    function randomNum(square) {
+      square.innerHTML = Math.floor((Math.random() * 9) + 1)
+    }
+
+    const row1 = document.querySelectorAll('.row1')
+    const row2 = document.querySelectorAll('.row2')
+    const row3 = document.querySelectorAll('.row3')
+    const row4 = document.querySelectorAll('.row4')
+    const row5 = document.querySelectorAll('.row5')
+    const row6 = document.querySelectorAll('.row6')
+    const row7 = document.querySelectorAll('.row7')
+    const row8 = document.querySelectorAll('.row8')
+    const row9 = document.querySelectorAll('.row9')
+
+    row1.forEach(randomNum)
+    row2.forEach(randomNum)
+    row3.forEach(randomNum)
+    row4.forEach(randomNum)
+    row5.forEach(randomNum)
+    row6.forEach(randomNum)
+    row7.forEach(randomNum)
+    row8.forEach(randomNum)
+    row9.forEach(randomNum)
+  }
 
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
@@ -31,7 +49,7 @@ function init() {
         nineCount = 1
       }
 
-      cell.innerHTML = i
+      // cell.innerHTML = i
       
       if ((i / 3) % 1 === 0) {
         cell.classList.add('left')
@@ -50,6 +68,7 @@ function init() {
     }
   }
   createGrid()
+  rowMaker()
 }
 
 window.addEventListener('DOMContentLoaded', init)
